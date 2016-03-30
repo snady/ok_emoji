45f71ea83faa471a0661e968a06fbd8201bbd018
Sandy Fang <sfang2016@gmail.com>
2016-03-29 23:43:18 -0400

    fixed

--- data/data.js
+++ data/data.js
@@ -1,6 +1,6 @@
 var text = 
 "
-{'PARENT BY GENDER': 
+{'DETAILED GENERATION': 
 {'Younger Millennial (18-25)': {'ind': 50.0, 'rep': 18.0, 'n': 2693.0, 'dem': 27.0, 'dk': 5.0}, 
 'Older Silent (79-86)': {'ind': 27.0, 'rep': 35.0, 'n': 1245.0, 'dem': 34.0, 'dk': 4.0}, 
 'Older Millennial (26-33)': {'ind': 47.0, 'rep': 18.0, 'n': 2622.0, 'dem': 28.0, 'dk': 7.0}, 
@@ -9,14 +9,16 @@ var text =
 'Younger Boomer (50-58)': {'ind': 36.0, 'rep': 26.0, 'n': 4414.0, 'dem': 32.0, 'dk': 6.0}, 
 'Younger Silent (69-78)': {'ind': 30.0, 'rep': 32.0, 'n': 3105.0, 'dem': 33.0, 'dk': 6.0}, 
 'Older Gen Xer (42-49)': {'ind': 37.0, 'rep': 23.0, 'n': 2880.0, 'dem': 33.0, 'dk': 7.0}}, 
-'COMMUNITY TYPE': {'$75,000 to $99,999': {'ind': 38.0, 'rep': 30.0, 'n': 2783.0, 'dem': 29.0, 'dk': 3.0}, 
+'DETAILED FAMILY INCOME': 
+{'$75,000 to $99,999': {'ind': 38.0, 'rep': 30.0, 'n': 2783.0, 'dem': 29.0, 'dk': 3.0}, 
 '<$30,000': {'ind': 42.0, 'rep': 17.0, 'n': 6995.0, 'dem': 35.0, 'dk': 7.0}, 
 '$30,000 to $39,999': {'ind': 43.0, 'rep': 19.0, 'n': 2283.0, 'dem': 33.0, 'dk': 4.0}, 
 '$100,000 to $149,999': {'ind': 37.0, 'rep': 30.0, 'n': 2566.0, 'dem': 30.0, 'dk': 3.0}, 
 '$50,000 to $74,999': {'ind': 38.0, 'rep': 27.0, 'n': 3483.0, 'dem': 30.0, 'dk': 4.0}, 
 '$40,000 to $49,999': {'ind': 40.0, 'rep': 24.0, 'n': 2045.0, 'dem': 33.0, 'dk': 4.0}, 
 '$150,000+': {'ind': 38.0, 'rep': 29.0, 'n': 2229.0, 'dem': 29.0, 'dk': 4.0}}, 
-'GENERATION': {'College men': {'ind': 45.0, 'rep': 27.0, 'n': 3343.0, 'dem': 24.0, 'dk': 4.0}, 
+'EDUCATION 4-WAY BY GENDER': 
+{'College men': {'ind': 45.0, 'rep': 27.0, 'n': 3343.0, 'dem': 24.0, 'dk': 4.0}, 
 'HS or less women': {'ind': 35.0, 'rep': 22.0, 'n': 3689.0, 'dem': 35.0, 'dk': 8.0}, 
 'Post graduate women': {'ind': 32.0, 'rep': 18.0, 'n': 1830.0, 'dem': 46.0, 'dk': 4.0}, 
 'College women': {'ind': 33.0, 'rep': 25.0, 'n': 2955.0, 'dem': 37.0, 'dk': 5.0}, 
@@ -24,7 +26,8 @@ var text =
 'Some college women': {'ind': 36.0, 'rep': 24.0, 'n': 3639.0, 'dem': 35.0, 'dk': 5.0}, 
 'Post graduate men': {'ind': 45.0, 'rep': 22.0, 'n': 2142.0, 'dem': 29.0, 'dk': 4.0}, 
 'HS or less men': {'ind': 44.0, 'rep': 22.0, 'n': 3996.0, 'dem': 27.0, 'dk': 6.0}}, 
-'EMPLOYED?': {'South Central (AL AR KY LA MS OK TN TX)': {'ind': 39.0, 'rep': 27.0, 'n': 4130.0, 'dem': 28.0, 'dk': 6.0}, 
+'REGION': 
+{'South Central (AL AR KY LA MS OK TN TX)': {'ind': 39.0, 'rep': 27.0, 'n': 4130.0, 'dem': 28.0, 'dk': 6.0}, 
 'Northeast': {'ind': 38.0, 'rep': 20.0, 'n': 4334.0, 'dem': 36.0, 'dk': 6.0}, 
 'Middle Atlantic (DE DC MD NJ NY PA)': {'ind': 34.0, 'rep': 20.0, 'n': 3757.0, 'dem': 39.0, 'dk': 6.0}, 
 'West': {'ind': 42.0, 'rep': 22.0, 'n': 5751.0, 'dem': 30.0, 'dk': 7.0}, 
@@ -36,10 +39,12 @@ var text =
 'Midwest': {'ind': 40.0, 'rep': 24.0, 'n': 5657.0, 'dem': 31.0, 'dk': 6.0}, 
 'New England (CT ME MA NH RI VT)': {'ind': 48.0, 'rep': 16.0, 'n': 1181.0, 'dem': 32.0, 'dk': 4.0}, 
 'South': {'ind': 39.0, 'rep': 25.0, 'n': 9268.0, 'dem': 30.0, 'dk': 5.0}}, 
-'RACE\\/ETHNICITY': {'Urban': {'ind': 39.0, 'rep': 18.0, 'n': 8118.0, 'dem': 38.0, 'dk': 6.0}, 
+'COMMUNITY TYPE': 
+{'Urban': {'ind': 39.0, 'rep': 18.0, 'n': 8118.0, 'dem': 38.0, 'dk': 6.0}, 
 'Suburban': {'ind': 40.0, 'rep': 25.0, 'n': 12065.0, 'dem': 29.0, 'dk': 5.0}, 
 'Rural': {'ind': 39.0, 'rep': 29.0, 'n': 4556.0, 'dem': 26.0, 'dk': 6.0}}, 
-'EDUCATION 4-WAY BY GENDER': {'Total Catholic': {'ind': 37.0, 'rep': 24.0, 'n': 5125.0, 'dem': 33.0, 'dk': 6.0}, 
+'RELIGIOUS TRADITION': 
+{'Total Catholic': {'ind': 37.0, 'rep': 24.0, 'n': 5125.0, 'dem': 33.0, 'dk': 6.0}, 
 'Atheist': {'ind': 47.0, 'rep': 6.0, 'n': 781.0, 'dem': 42.0, 'dk': 5.0}, 
 'White Non-Hisp Mainline Protestant': {'ind': 38.0, 'rep': 30.0, 'n': 4144.0, 'dem': 26.0, 'dk': 6.0}, 
 'Hispanic Catholic': {'ind': 41.0, 'rep': 12.0, 'n': 1283.0, 'dem': 38.0, 'dk': 9.0}, 
@@ -50,10 +55,12 @@ var text =
 'White Non-Hisp Evangelical Protestant': {'ind': 34.0, 'rep': 46.0, 'n': 4814.0, 'dem': 14.0, 'dk': 5.0}, 
 'Total Unaffiliated': {'ind': 50.0, 'rep': 10.0, 'n': 4983.0, 'dem': 34.0, 'dk': 6.0}, 
 'Black Protestant': {'ind': 22.0, 'rep': 5.0, 'n': 1812.0, 'dem': 68.0, 'dk': 4.0}}, 
-'MARITAL STATUS BY GENDER': {'$75,000+': {'ind': 38.0, 'rep': 30.0, 'n': 7578.0, 'dem': 29.0, 'dk': 3.0}, 
+'FAMILY INCOME': 
+{'$75,000+': {'ind': 38.0, 'rep': 30.0, 'n': 7578.0, 'dem': 29.0, 'dk': 3.0}, 
 '<$30,000': {'ind': 42.0, 'rep': 17.0, 'n': 6995.0, 'dem': 35.0, 'dk': 7.0}, 
 '$30,000-$74,999': {'ind': 40.0, 'rep': 24.0, 'n': 7811.0, 'dem': 32.0, 'dk': 4.0}}, 
-'GENDER': {'Silent women': {'ind': 25.0, 'rep': 31.0, 'n': 2437.0, 'dem': 39.0, 'dk': 6.0}, 
+'GENERATION BY GENDER': 
+{'Silent women': {'ind': 25.0, 'rep': 31.0, 'n': 2437.0, 'dem': 39.0, 'dk': 6.0}, 
 'Boomer women': {'ind': 31.0, 'rep': 24.0, 'n': 4657.0, 'dem': 39.0, 'dk': 5.0}, 
 'Silent men': {'ind': 35.0, 'rep': 35.0, 'n': 1913.0, 'dem': 26.0, 'dk': 4.0}, 
 'Boomer men': {'ind': 39.0, 'rep': 26.0, 'n': 4735.0, 'dem': 29.0, 'dk': 5.0}, 
@@ -61,42 +68,53 @@ var text =
 'Millennial men': {'ind': 53.0, 'rep': 18.0, 'n': 3088.0, 'dem': 23.0, 'dk': 5.0}, 
 'Gen Xer men': {'ind': 45.0, 'rep': 23.0, 'n': 2823.0, 'dem': 26.0, 'dk': 6.0}, 
 'Millennial women': {'ind': 43.0, 'rep': 18.0, 'n': 2227.0, 'dem': 33.0, 'dk': 6.0}}, 
-'REGION': {'College grad men': {'ind': 45.0, 'rep': 25.0, 'n': 5485.0, 'dem': 26.0, 'dk': 4.0}, 
+'EDUCATION 2-WAY BY GENDER': 
+{'College grad men': {'ind': 45.0, 'rep': 25.0, 'n': 5485.0, 'dem': 26.0, 'dk': 4.0}, 
 'Some coll or less women': {'ind': 35.0, 'rep': 23.0, 'n': 7328.0, 'dem': 35.0, 'dk': 7.0}, 
 'Some coll or less men': {'ind': 45.0, 'rep': 23.0, 'n': 7277.0, 'dem': 26.0, 'dk': 6.0}, 
 'College grad women': {'ind': 33.0, 'rep': 22.0, 'n': 4785.0, 'dem': 41.0, 'dk': 4.0}}, 
-'EDUCATION 2-WAY BY GENDER': {'Yes': {'ind': 42.0, 'rep': 21.0, 'n': 1207.0, 'dem': 30.0, 'dk': 7.0}, 
+'PARENT OR GUARDIAN': 
+{'Yes': {'ind': 42.0, 'rep': 21.0, 'n': 1207.0, 'dem': 30.0, 'dk': 7.0}, 
 'No': {'ind': 40.0, 'rep': 23.0, 'n': 3618.0, 'dem': 31.0, 'dk': 6.0}}, 
-'PARENT OR GUARDIAN': {'Hispanic': {'ind': 44.0, 'rep': 13.0, 'n': 2594.0, 'dem': 34.0, 'dk': 9.0}, 
+'PARENT OR GUARDIAN': 
+{'Hispanic': {'ind': 44.0, 'rep': 13.0, 'n': 2594.0, 'dem': 34.0, 'dk': 9.0}, 
 'Asian, non-Hispanic (English-speaking only)': {'ind': 46.0, 'rep': 11.0, 'n': 641.0, 'dem': 37.0, 'dk': 6.0}, 
 'Black, non-Hispanic': {'ind': 26.0, 'rep': 5.0, 'n': 2496.0, 'dem': 64.0, 'dk': 5.0}, 
 'White, non-Hispanic': {'ind': 40.0, 'rep': 30.0, 'n': 17823.0, 'dem': 25.0, 'dk': 5.0}}, 
-'DETAILED FAMILY INCOME': {'Unmarried': {'ind': 43.0, 'rep': 17.0, 'n': 6168.0, 'dem': 34.0, 'dk': 6.0}, 
+'MARITAL STATUS': 
+{'Unmarried': {'ind': 43.0, 'rep': 17.0, 'n': 6168.0, 'dem': 34.0, 'dk': 6.0}, 
 'Married': {'ind': 39.0, 'rep': 28.0, 'n': 6758.0, 'dem': 27.0, 'dk': 6.0}}, 
-'MARITAL STATUS': {'Non-parent women': {'ind': 35.0, 'rep': 22.0, 'n': 1784.0, 'dem': 36.0, 'dk': 6.0}, 
+'PARENT BY GENDER': 
+{'Non-parent women': {'ind': 35.0, 'rep': 22.0, 'n': 1784.0, 'dem': 36.0, 'dk': 6.0}, 
 'Parent men': {'ind': 44.0, 'rep': 24.0, 'n': 606.0, 'dem': 27.0, 'dk': 6.0}, 
 'Non-parent men': {'ind': 45.0, 'rep': 24.0, 'n': 1834.0, 'dem': 26.0, 'dk': 5.0}, 
 'Parent women': {'ind': 40.0, 'rep': 19.0, 'n': 601.0, 'dem': 33.0, 'dk': 9.0}}, 
-'DETAILED MARITAL STATUS': {'Silent (69-86)': {'ind': 29.0, 'rep': 33.0, 'n': 4350.0, 'dem': 33.0, 'dk': 5.0}, 
+'GENERATION': 
+{'Silent (69-86)': {'ind': 29.0, 'rep': 33.0, 'n': 4350.0, 'dem': 33.0, 'dk': 5.0}, 
 'Baby-Boomer (50-68)': {'ind': 35.0, 'rep': 25.0, 'n': 9392.0, 'dem': 34.0, 'dk': 5.0}, 
 'Generation X (34-49)': {'ind': 40.0, 'rep': 22.0, 'n': 5206.0, 'dem': 32.0, 'dk': 6.0}, 
 'Millennial (ages 18-33 in 2014)': {'ind': 48.0, 'rep': 18.0, 'n': 5315.0, 'dem': 28.0, 'dk': 6.0}}, 
-'DETAILED GENERATION': {'Unmarried women': {'ind': 38.0, 'rep': 17.0, 'n': 3309.0, 'dem': 39.0, 'dk': 6.0}, 
+'MARITAL STATUS BY GENDER': 
+{'Unmarried women': {'ind': 38.0, 'rep': 17.0, 'n': 3309.0, 'dem': 39.0, 'dk': 6.0}, 
 'Married women': {'ind': 35.0, 'rep': 28.0, 'n': 3043.0, 'dem': 31.0, 'dk': 6.0}, 
 'Unmarried men': {'ind': 50.0, 'rep': 17.0, 'n': 2859.0, 'dem': 28.0, 'dk': 5.0}, 
 'Married men': {'ind': 43.0, 'rep': 28.0, 'n': 3715.0, 'dem': 23.0, 'dk': 6.0}}, 
-'GENERATION BY GENDER': {'Never married': {'ind': 47.0, 'rep': 15.0, 'n': 2418.0, 'dem': 33.0, 'dk': 5.0}, 
+'DETAILED MARITAL STATUS': 
+{'Never married': {'ind': 47.0, 'rep': 15.0, 'n': 2418.0, 'dem': 33.0, 'dk': 5.0}, 
 'Widowed': {'ind': 30.0, 'rep': 27.0, 'n': 1231.0, 'dem': 38.0, 'dk': 5.0}, 
 'Married': {'ind': 39.0, 'rep': 28.0, 'n': 6758.0, 'dem': 27.0, 'dk': 6.0}, 
 'Divorced\\/Separated': {'ind': 39.0, 'rep': 17.0, 'n': 1740.0, 'dem': 37.0, 'dk': 7.0}, 
 'Living with a partner': {'ind': 50.0, 'rep': 13.0, 'n': 779.0, 'dem': 32.0, 'dk': 6.0}}, 
-'EDUCATION': {'Men': {'ind': 45.0, 'rep': 24.0, 'n': 12843.0, 'dem': 26.0, 'dk': 5.0}, 
+'GENDER': 
+{'Men': {'ind': 45.0, 'rep': 24.0, 'n': 12843.0, 'dem': 26.0, 'dk': 5.0}, 
 'Women': {'ind': 35.0, 'rep': 23.0, 'n': 12167.0, 'dem': 37.0, 'dk': 6.0}}, 
-'FAMILY INCOME': {'Part-time': {'ind': 39.0, 'rep': 22.0, 'n': 531.0, 'dem': 31.0, 'dk': 9.0}, 
+'EMPLOYED': 
+{'Part-time': {'ind': 39.0, 'rep': 22.0, 'n': 531.0, 'dem': 31.0, 'dk': 9.0}, 
 'Yes': {'ind': 40.0, 'rep': 23.0, 'n': 2056.0, 'dem': 30.0, 'dk': 7.0}, 
 'Full-time': {'ind': 41.0, 'rep': 24.0, 'n': 1525.0, 'dem': 30.0, 'dk': 6.0}, 
 'No': {'ind': 34.0, 'rep': 22.0, 'n': 1238.0, 'dem': 35.0, 'dk': 9.0}}, 
-'RELIGIOUS TRADITION': {'High school or less': {'ind': 40.0, 'rep': 22.0, 'n': 7685.0, 'dem': 31.0, 'dk': 7.0}, 
+'EDUCATION': 
+{'High school or less': {'ind': 40.0, 'rep': 22.0, 'n': 7685.0, 'dem': 31.0, 'dk': 7.0}, 
 'Some college or less': {'ind': 40.0, 'rep': 23.0, 'n': 14605.0, 'dem': 31.0, 'dk': 6.0}, 
 'Some college': {'ind': 40.0, 'rep': 25.0, 'n': 6920.0, 'dem': 30.0, 'dk': 5.0}, 
 'College graduate': {'ind': 39.0, 'rep': 26.0, 'n': 6298.0, 'dem': 31.0, 'dk': 4.0}, 
